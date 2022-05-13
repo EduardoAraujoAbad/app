b89d5de055f99b2d322aa69b946488b05ca0c869
EduardoAraujoAbad <edaraujoab@uide.edu.ec>
2022-05-12 21:02:09 -0500

    12/5/2022

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.example.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.app.databinding.ActivityMainBinding
+import com.example.app.principal.Principal
 
 class MainActivity : AppCompatActivity() {
     private lateinit var views: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         views = ActivityMainBinding.inflate(layoutInflater)
         initializationListeners()
         setContentView(views.root)
 
     }
-    private fun initializationListeners(){
-        views.button.setOnClickListener{
-            Toast.makeText(this,"El usuario es:"+" "+ views.userInput.text+" "+"y su contrasena es"+" "+views.passwordInput.text, Toast.LENGTH_SHORT).show()
+
+    private fun initializationListeners() {
+        views.button.setOnClickListener {
+            val userText = views.userInput.text.toString()
+            val passwordText = views.passwordInput.text.toString()
+
+            userText?.let {
+                passwordText.let { itp ->
+                    if (it.equals("edu") && itp.equals("1234"))
+                        goToPrincipal(userText)
+                }
+            }
         }
+    }
+
+    private fun goToPrincipal(userText: String) {
+        var intent = Intent(this, Principal::class.java)
+        intent.putExtra("name_user", userText)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/app/principal/Principal.kt
+++ app/src/main/java/com/example/app/principal/Principal.kt
@@ -1,12 +1,23 @@
 package com.example.app.principal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RemoteViews
 import com.example.app.R
+import com.example.app.databinding.ActivityMainBinding
+import com.example.app.databinding.ActivityPrincipalBinding
 
 class Principal : AppCompatActivity() {
+    private lateinit var views: ActivityPrincipalBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
+        views= ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        showName(intent)
+    }
+    private fun showName(intent: Intent){
+        val username = intent.getStringExtra("name_user")
+        views.userInputPrincipal.setText(username)
     }
 }
