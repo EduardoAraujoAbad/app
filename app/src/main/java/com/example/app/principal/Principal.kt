eae37a7cfeaca0ab594a9c9a42c852dc65de6319
EduardoAraujoAbad <edaraujoab@uide.edu.ec>
2022-05-16 11:10:58 -0500

    16/5/2022-new branch

--- app/src/main/java/com/example/app/principal/Principal.kt
+++ app/src/main/java/com/example/app/principal/Principal.kt
@@ -1,14 +1,21 @@
 package com.example.app.principal
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.drawable.AnimationDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RemoteViews
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.example.app.R
 import com.example.app.databinding.ActivityMainBinding
 import com.example.app.databinding.ActivityPrincipalBinding
+import java.util.jar.Manifest
 
 class Principal : AppCompatActivity() {
     private lateinit var views: ActivityPrincipalBinding
@@ -24,8 +31,54 @@ class Principal : AppCompatActivity() {
         animationDrawable.setExitFadeDuration(4000)
         animationDrawable.start()
 
+        views.btnProfilepicture.setOnClickListener{ requestPermissions() }
 
     }
+
+    private fun requestPermissions() {
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
+            when{
+                ContextCompat.checkSelfPermission(
+                   this,
+                   android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )==PackageManager.PERMISSION_GRANTED ->{
+                    pickPhotoFromGallery()
+                }
+                else -> requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+            }
+        }else{
+            pickPhotoFromGallery()
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted->
+        if (isGranted){
+            pickPhotoFromGallery()
+        }else{
+            Toast.makeText(this,"Necesitas habilitar los permisos",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val startforActivityGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result->
+        if (result.resultCode == Activity.RESULT_OK){
+            val data = result.data?.data
+            views.userimage.setImageURI(data)
+        }
+    }
+
+    private fun pickPhotoFromGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type="image/*"
+        startforActivityGallery.launch(intent)
+
+    }
+
     private fun showName(intent: Intent){
         val username = intent.getStringExtra("name_user")
         views.userInputPrincipal.setText(username)
