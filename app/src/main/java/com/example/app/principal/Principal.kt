a52c43f3cb60878ca02d3424d5ed0e2285e18ea8
EduardoAraujoAbad <edaraujoab@uide.edu.ec>
2022-05-15 20:39:31 -0500

    13/5/2022

--- app/src/main/java/com/example/app/principal/Principal.kt
+++ app/src/main/java/com/example/app/principal/Principal.kt
@@ -1,9 +1,11 @@
 package com.example.app.principal
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RemoteViews
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.app.R
 import com.example.app.databinding.ActivityMainBinding
 import com.example.app.databinding.ActivityPrincipalBinding
@@ -15,6 +17,12 @@ class Principal : AppCompatActivity() {
         views= ActivityPrincipalBinding.inflate(layoutInflater)
         setContentView(views.root)
         showName(intent)
+
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.principal_layout)
+        val animationDrawable: AnimationDrawable =constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
     }
     private fun showName(intent: Intent){
         val username = intent.getStringExtra("name_user")
